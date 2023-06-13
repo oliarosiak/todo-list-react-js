b4cf40e73f6e8294a860cf9acebd8c03ead99dbd
Olia Rosiak <olia.rosiak@gmail.com>
2023-06-13 14:35:03 +0300

    initial basic markup and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,47 @@
-
+import { useState } from "react";
+import { GrAddCircle } from "react-icons/gr";
+import { RiDeleteBin5Line } from "react-icons/ri";
 import css from './App.module.css';
 
-export const App = () => {
+export default function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const inputTodoHandler = (event) => {    
+    setNewTodo(event.target.value)
+  }
+
+  const formTodoHandler = (event) => {
+    event.preventDefault();  
+    setTodoList(prevTodo => [...prevTodo, newTodo]);    
+    setNewTodo('');    
+  }
+
   return (
     <div className={css.App} >
-      React homework template
+      <h1>ToDo List</h1>
+      <form onSubmit={formTodoHandler} className={css.TodoForm} >
+        <label>          
+          <input
+            type="text"
+            placeholder="Add New ToDo"
+            value={newTodo}
+            onChange={inputTodoHandler}
+          ></input>
+        </label>
+        <button type='submit'><GrAddCircle /></button>
+      </form>
+
+      <ul className={css.TodoList} >
+        {todoList.map(todo => (
+          <li key={todo} className={css.TodoListItem} >
+            <input type='checkbox' />
+            <span>{todo}</span>
+            <button type='button'><RiDeleteBin5Line /></button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+
+}
