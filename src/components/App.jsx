6ab29b2784e06abd0cb6d67d99ea38c92b8ac0ae
Olia Rosiak <olia.rosiak@gmail.com>
2023-06-13 12:39:32 +0300

    add propTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,9 @@
+
+import css from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div className={css.App} >
       React homework template
     </div>
   );
